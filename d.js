6db556da5b62861269bb907f6b1e2a6807accabe
burgerman1222 <burgerman1222@z.org>
2026-05-12 14:35:48 +0000

    Update d.js

--- d.js
+++ d.js
@@ -227,17 +227,21 @@ const indexHtml = `<!DOCTYPE html>
     });
 
     socket.on('connect', () => {
-      appendLine('[System] Connected to server', 'system');
-      const pathName = window.location.pathname;
-      if (pathName === 'master') {
-        socket.emit('register-master');
-      } else {
-        const uuid = (pathName || '/').substring(1);
-        if (uuid && uuid !== '') {
-          socket.emit('register-viewer', uuid);
-        }
-      }
-    });
+  appendLine('[System] Connected to server', 'system');
+  const pathName = window.location.pathname;
+  if (pathName === '/master') {
+    socket.emit('register-master');
+  } else {
+    const uuid = (pathName || '/').substring(1);
+    if (uuid && uuid !== '') {
+      socket.emit('register-viewer', uuid);
+    } else {
+      // If no UUID provided, register as a new client
+      socket.emit('register-client');
+    }
+  }
+});
+
 
     socket.on('connect_error', (error) => { appendLine('[System] Connection error: ' + (error.message || 'Unknown error'), 'error'); });
     socket.on('disconnect', (reason) => { appendLine('[System] Disconnected from server: ' + reason, 'error'); });
@@ -306,8 +310,9 @@ const indexHtml = `<!DOCTYPE html>
 
 fs.writeFileSync(path.join(publicDir, 'index.html'), indexHtml);
 
+app.use(express.static(publicDir));
 
-app.get('/masterzxcvbnm', (req, res) => {
+app.get('/master', (req, res) => {
   res.sendFile(path.join(publicDir, 'index.html'));
 });
 
